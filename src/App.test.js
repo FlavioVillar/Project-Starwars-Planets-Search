aa50ffadd4c59180e4a6902fe148ad29641a5450
FlavioVillar <flavio_villar@hotmail.com>
2022-04-18 15:41:25 -0300

    Correções de Lint

--- src/App.test.js
+++ src/App.test.js
@@ -35,7 +35,7 @@ const mockFetch = () => {
     }));
 };
 
-describe.only('1 - Faça uma requisição para o endpoint `/planets` da API de Star Wars e preencha uma tabela com os dados retornados, com exceção dos da coluna `residents`', () => {
+describe('1 - Faça uma requisição para o endpoint `/planets` da API de Star Wars e preencha uma tabela com os dados retornados, com exceção dos da coluna `residents`', () => {
   beforeAll(mockFetch);
   beforeEach(cleanup);
 
@@ -342,7 +342,7 @@ describe('4 - Implemente múltiplos filtros numéricos', () => {
   });
 });
 
-describe('5 - Não utilize filtros repetidos', () => {
+describe.only('5 - Não utilize filtros repetidos', () => {
   beforeAll(mockFetch);
   beforeEach(cleanup);
 
